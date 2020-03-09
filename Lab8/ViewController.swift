495c887fa50799d89009d5602e2ad56ef4070731
Marlo <m.osborne.subasic@gmail.com>
2020-03-09 15:21:33 -0700

    lab 8 done

--- Lab8/ViewController.swift
+++ Lab8/ViewController.swift
@@ -12,14 +12,18 @@ import RealityKit
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
+    @IBOutlet weak var orbitButton: UIButton!
+    
+    // Load the "Box" scene from the "Experience" Reality File
+    let boxAnchor = try! Experience.loadBox()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
+    }
+    @IBAction func orbitPressed(_ sender: Any) {
+        boxAnchor.notifications.startNotification.post()
     }
 }
